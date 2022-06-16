9a9ed2c7c6cb8530a51b0843564ab0463486bd81
Whatsup <whatsup@lemonbase.com>
2022-06-16 19:37:57 +0900

    feat: eslintrc 추가
    - eslint 테스트 파일 생성

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,11 @@
+import React from "react";
+
 function HomePage(): JSX.Element {
+  [1, 2, 3].map((x) => {
+    const y = x + 1;
+    return x * y;
+  });
+
   return <div>Welcome to Next.js!!!!!</div>;
 }
 
