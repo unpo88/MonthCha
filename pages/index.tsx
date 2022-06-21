cf0b9b5aaa935f883f768a901e3e83712287274d
myeongseung-han <hms3512@khu.ac.kr>
2022-06-21 20:01:32 +0900

    feat: recoil 테스트

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { textState, charCountState } from "./states";
+
+function TextInput() {
+  const [text, setText] = useRecoilState(textState);
+
+  const onChange = (event: any) => {
+    setText(event.target.value);
+  };
+
+  return (
+    <div>
+      <input type="text" value={text} onChange={onChange} />
+      <br />
+      Echo: {text}
+    </div>
+  );
+}
+
+function CharacterCount() {
+  const count = useRecoilValue(charCountState);
+
+  return <>Character Count: {count}</>;
+}
+
+function CharacterCounter() {
+  return (
+    <div>
+      <TextInput />
+      <CharacterCount />
+    </div>
+  );
+}
 
 function HomePage(): JSX.Element {
-  return <>Welcome to Next.js!!!!!</>;
+  return <CharacterCounter />;
 }
 
 export default HomePage;
